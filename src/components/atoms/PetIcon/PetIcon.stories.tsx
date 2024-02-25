90510c5e20df747902674d8189ec31fd6cc73742
ryujinzz <kdo2240k@naver.com>
2024-02-26 00:56:30 +0900

    [modify] PetIcon 수정
     PetIcon 소,중,대형견 수정

--- src/components/atoms/PetIcon/PetIcon.stories.tsx
+++ src/components/atoms/PetIcon/PetIcon.stories.tsx
@@ -8,9 +8,6 @@ export default {
 /**@type{import('@storybook/react').StoryObj} */
 export const 기본표시 = {
   args: {
-    widthHeight: {
-      blockSize: 20,
-      inlineSize: 10,
-    },
+    dogSize: '중형견',
   },
 };
--- src/components/atoms/PetIcon/PetIcon.tsx
+++ src/components/atoms/PetIcon/PetIcon.tsx
@@ -1,67 +1,44 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 interface PetProps {
-  $inlineSize: number;
-  $blockSize: number;
+  $dogSize: string;
 }
 
 const Pet = styled.img.attrs((props) => ({
   src: props.src,
   alt: props.alt,
 }))<PetProps>`
-  inline-size: ${(props) => {
-    switch (props.$inlineSize) {
-      case 26:
-        return 26;
-      case 20:
-        return 20;
-      case 16:
-        return 16;
-      default:
-        return props.$inlineSize;
+  ${(props) => {
+    if (props.$dogSize === '대형견') {
+      return css`
+        inline-size: 26px;
+        block-size: 12px;
+      `;
     }
-  }}px;
-
-  block-size: ${(props) => {
-    switch (props.$blockSize) {
-      case 12:
-        return 12;
-      case 10:
-        return 10;
-      case 8:
-        return 8;
-      default:
-        return props.$blockSize;
+    if (props.$dogSize === '중형견') {
+      return css`
+        inline-size: 20px;
+        block-size: 10px;
+      `;
     }
-  }}px;
+    if (props.$dogSize === '소형견') {
+      return css`
+        inline-size: 16px;
+        block-size: 8px;
+      `;
+    }
+  }}
   padding-inline: 2px;
   padding-block: 4px;
 `;
 
 interface PetIconProps {
-  widthHeight: { blockSize: number; inlineSize: number };
+  // Union Type : 또는
+  dogSize: '대형견' | '중형견' | '소형견';
 }
 
-const PetIcon = ({ widthHeight }: PetIconProps) => {
-  const { blockSize = 20, inlineSize = 10 } = widthHeight;
-
-  let dogSize;
-  if (blockSize <= 20) {
-    dogSize = '소형견';
-  } else if (blockSize <= 30) {
-    dogSize = '중형견';
-  } else {
-    dogSize = '대형견';
-  }
-
-  return (
-    <Pet
-      src={'/images/BigDog.svg'}
-      $blockSize={blockSize}
-      $inlineSize={inlineSize}
-      alt={dogSize}
-    />
-  );
+const PetIcon = ({ dogSize }: PetIconProps) => {
+  return <Pet src={'/images/BigDog.svg'} $dogSize={dogSize} alt={dogSize} />;
 };
 
 export default PetIcon;
