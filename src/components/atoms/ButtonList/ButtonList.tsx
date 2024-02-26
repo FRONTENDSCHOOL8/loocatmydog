da7c347197a04e3bb9e3ad18333f6807715ecc38
vHwav <dandivan0708@gmail.com>
2024-02-26 14:08:58 +0900

    buttonList 중간저장

--- src/components/atoms/ButtonList/ButtonList.tsx
+++ src/components/atoms/ButtonList/ButtonList.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 const StyledButtonList = styled.div`
-    
+  inline-size: 90px;
+  block-size: 60px;
+  display: flex;
+  flex-direction: column;
 `;
-
 const ButtonList = () => {
   return <StyledButtonList></StyledButtonList>;
 };
