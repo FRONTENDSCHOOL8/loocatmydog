3cb4966e2e5a1e8bc8f263e6b2992e8b1a757084
ryujinzz <kdo2240k@naver.com>
2024-02-26 22:36:27 +0900

    [feat] 예약탭 구현
    molecules 예약탭 구현 및 molecules 폴더 이동 수정

---
+++ src/components/molecules/Tab/Tab.stories.tsx
@@ -0,0 +1,13 @@
+import Tab from './Tab';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: Tab,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {
+  args: {
+    mode: 'present',
+  },
+};
