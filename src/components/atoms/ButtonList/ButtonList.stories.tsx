52900e2a5286ada6e1771b9d9ec847fdcd652f82
vHwav <dandivan0708@gmail.com>
2024-02-26 13:50:04 +0900

    buttonList 중간저장

---
+++ src/components/atoms/ButtonList/ButtonList.stories.tsx
@@ -0,0 +1,9 @@
+import ButtonList from './ButtonList';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: ButtonList,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
---
+++ src/components/atoms/ButtonList/ButtonList.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledButtonList = styled.div`
+    
+`;
+
+const ButtonList = () => {
+  return <StyledButtonList></StyledButtonList>;
+};
+
+export default ButtonList;
