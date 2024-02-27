38538234f9a0d79863bb4880cf28287ef14788e3
jykim29 <skt13181@naver.com>
2024-02-27 15:34:24 +0900

    [Feat] Header 컴포넌트 구현
    - 헤더 type
    ('step' | 'main' | 'logo' | 'post' | 'place' | 'popup' | 'back')
    에 따라 디자인을 다르게 렌더링하도록 설계

---
+++ src/components/molecules/SearchInput/SearchInput.stories.tsx
@@ -0,0 +1,9 @@
+import SearchInput from './SearchInput';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: SearchInput,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
