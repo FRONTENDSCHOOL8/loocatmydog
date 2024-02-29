0a53ba941b74b056fc8f6fea7fa0dd100e5aff1f
ryujinzz <kdo2240k@naver.com>
2024-02-29 11:12:23 +0900

    [Modify] NameInput 을 FormInput으로 수정
    - 내용도 as 추가
    - props 추가
    - label 숨김 추가

--- src/components/molecules/FormInput/FormInput.stories.tsx
+++ src/components/molecules/FormInput/FormInput.stories.tsx
@@ -1,13 +1,16 @@
-import NameInput from './NameInput';
+import FormInput from './FormInput';
 
 /**@type{import('@storybook/react').Meta} */
 export default {
-  component: NameInput,
+  component: FormInput,
 };
 
 /**@type{import('@storybook/react').StoryObj} */
 export const 기본표시 = {
   args: {
     mode: 'register',
+    children: '이름',
+    placeholder: '실명 입력',
+    hiddenLabel: false,
   },
 };
