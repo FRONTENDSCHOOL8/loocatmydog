91ccedeb26a892a17af4422cc7b6aa7b6ec42cd0
vHwav <dandivan0708@gmail.com>
2024-02-29 16:31:27 +0900

    [Modify] ProfileCard 수정

--- src/components/molecules/AnimalRateInput/AnimalRateInput.tsx
+++ src/components/molecules/AnimalRateInput/AnimalRateInput.tsx
@@ -1,44 +1,19 @@
-import CheckBox from '@/components/atoms/CheckBox/CheckBox';
 import styled from 'styled-components';
+import CheckBox from './../../atoms/CheckBox/CheckBox';
 
 const StyledAnimalRateInput = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-  column-gap: 15px;
-  padding-block: 8px;
-  inline-size: 100%;
-  border: 1px solid black;
 
-  & .span-title {
-    ${(props) => props.theme.fontStyles.textRegularMd}
-    color: ${(props) => props.theme.colors.textBlack};
+  .inputTextWrapper {
+    flex-grow: 1;
   }
-
-  & .inputTextWrapper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    /* flex-grow: 1; */
-
-    & input {
-      vertical-align: top;
-      ${(props) => props.theme.fontStyles.textRegularMd}
-      color: ${(props) => props.theme.colors.textBlack};
-    }
-    & input::placeholder {
-      ${(props) => props.theme.fontStyles.textRegularMd}
-      color: ${(props) => props.theme.colors.textDarkGray};
-    }
-
-    & span {
-      ${(props) => props.theme.fontStyles.textRegularMd}
-      color: ${(props) => props.theme.colors.textBlack};
-    }
-  }
-
-  & .input-animalRate {
-    border: none;
+`;
+const StyledCheckBox = styled(CheckBox)`
+  fontsize: '0.75rem';
+  display: flex;
+  justify-content: end;
+  margin-inline-start: auto;
+  & label {
   }
 `;
 
@@ -59,6 +34,7 @@ const AnimalRateInput = () => {
         />
         <span>원</span>
       </div>
+
       <CheckBox style={{ fontSize: '0.75rem' }}>예약불가</CheckBox>
     </StyledAnimalRateInput>
   );
