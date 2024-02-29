0ee395d85df420a04a767d153359778c741015e1
ParkjiDev <parkjidev@gmail.com>
2024-02-29 21:23:09 +0900

    [Modify] StarRating 컴포넌트(별점) 수정
    - alt 추가
    - fill prop의 기본값 추가
    - img 요소에 ...restProps 추가
    
    - #15

--- src/components/atoms/StarRating/StarRating.tsx
+++ src/components/atoms/StarRating/StarRating.tsx
@@ -2,13 +2,21 @@ interface StarRatingProps {
   fill?: boolean;
 }
 
-const StarRating = ({ fill }: StarRatingProps) => {
+const StarRating = ({ fill = true, ...restProps }: StarRatingProps) => {
   let star;
 
   if (fill) {
-    star = <img src="/images/star-fill.svg" />;
+    star = (
+      <img
+        src="/images/star-fill.svg"
+        alt="노란색의 채워진 별"
+        {...restProps}
+      />
+    );
   } else {
-    star = <img src="/images/star.svg" />;
+    star = (
+      <img src="/images/star.svg" alt="노란색의 속이 빈 별" {...restProps} />
+    );
   }
 
   return <>{star}</>;
