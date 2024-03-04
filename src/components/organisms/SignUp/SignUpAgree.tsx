a49a92b73afd1ed1a8a1960b34771e951b4ccf24
ParkjiDev <parkjidev@gmail.com>
2024-03-04 14:54:05 +0900

    [Feat] 회원가입 약관동의 컴포넌트 구현
    - 개별 동의 체크박스 상태 관리
    - 전체 동의 체크박스 상태 관리
    - 버튼 활성화 상태 관리
    
    - #100

--- src/components/organisms/SignUp/SignUpAgree.tsx
+++ src/components/organisms/SignUp/SignUpAgree.tsx
@@ -1,12 +1,11 @@
 import Button from '@/components/atoms/Button/Button';
 import CheckBox from '@/components/atoms/CheckBox/CheckBox';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const StyledSignUpAgree = styled.div`
   padding-inline: 20px;
   padding-block-start: 40px;
-  /* background-color: pink;
-  border: 1px solid red; */
 
   & .p-signUp {
     margin-block-end: 5px;
@@ -47,34 +46,117 @@ const StyledSignUpAgree = styled.div`
 `;
 
 const SignUpAgree = () => {
+  // 이용약관 동의 초기값 false
+  const initialValue: { [key: number]: boolean } = {
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+  };
+
+  // 이용약관 동의(개별) state
+  const [isChecked, setIsChecked] = useState(initialValue);
+  // 이용약관 전체 동의 state
+  const [isAllChecked, setIsAllChecked] = useState(false);
+  // '다음으로' 버튼 활성 여부 state
+  const [isActive, setIsActive] = useState(false);
+
+  // 이용약관 개별 동의 렌더링에 따라 전체 동의 state와 버튼 state 조절
+  useEffect(() => {
+    if (Object.values(isChecked).indexOf(false) === -1) {
+      setIsAllChecked(true);
+      setIsActive(true);
+    } else {
+      setIsAllChecked(false);
+      setIsActive(false);
+    }
+  }, [isChecked]);
+
+  // 이용약관 개별 동의 체크박스 클릭
+  const handleCheck = (index: number) => {
+    setIsChecked((prevState) => ({
+      ...prevState,
+      [index]: !isChecked[index],
+    }));
+  };
+
+  // 이용약관 전체 동의 체크박스 클릭
+  const handleAllCheck = () => {
+    const allChecked = {
+      0: true,
+      1: true,
+      2: true,
+      3: true,
+    };
+
+    const allNotChecked = {
+      0: false,
+      1: false,
+      2: false,
+      3: false,
+    };
+
+    if (isAllChecked) {
+      setIsChecked(allNotChecked);
+      setIsActive(false);
+    } else {
+      setIsChecked(allChecked);
+      setIsActive(true);
+    }
+
+    setIsAllChecked((prevState) => !prevState);
+  };
+
   return (
     <StyledSignUpAgree>
       <p className="p-signUp">어서오세요,</p>
       <p className="p-signUp">약관동의가 필요해요</p>
       <div className="div-agreeAll">
-        <CheckBox reservation={false}>이용약관 전체 동의</CheckBox>
+        <CheckBox
+          reservation={false}
+          isChecked={isAllChecked}
+          onChange={handleAllCheck}
+        >
+          이용약관 전체 동의
+        </CheckBox>
       </div>
       <div className="div-line"></div>
       <div className="div-checkBoxList">
-        <CheckBox reservation={false}>
+        <CheckBox
+          reservation={false}
+          isChecked={isChecked[0]}
+          onChange={() => handleCheck(0)}
+        >
           봐주개냥 서비스 이용약관 동의(필수)
         </CheckBox>
         <span className="span-checkBoxList">보기</span>
       </div>
       <div className="div-checkBoxList">
-        <CheckBox reservation={false}>
+        <CheckBox
+          reservation={false}
+          isChecked={isChecked[1]}
+          onChange={() => handleCheck(1)}
+        >
           개인정보 수집 및 이용 동의(필수)
         </CheckBox>
         <span className="span-checkBoxList">보기</span>
       </div>
       <div className="div-checkBoxList">
-        <CheckBox reservation={false}>
+        <CheckBox
+          reservation={false}
+          isChecked={isChecked[2]}
+          onChange={() => handleCheck(2)}
+        >
           안전보상 프로그램 약관 동의(필수)
         </CheckBox>
         <span className="span-checkBoxList">보기</span>
       </div>
       <div className="div-checkBoxList">
-        <CheckBox reservation={false}>
+        <CheckBox
+          reservation={false}
+          isChecked={isChecked[3]}
+          onChange={() => handleCheck(3)}
+        >
           위치기반 서비스 이용약관 동의(필수)
         </CheckBox>
         <span className="span-checkBoxList">보기</span>
@@ -85,9 +167,16 @@ const SignUpAgree = () => {
           alt="한 쪽 발을 들고 있는 강아지 사진"
         />
       </div>
-      <Button size={'100%'} mode={'disabled'}>
-        다음으로
-      </Button>
+      {isActive && (
+        <Button size={'100%'} mode={'normal'}>
+          다음으로
+        </Button>
+      )}
+      {!isActive && (
+        <Button size={'100%'} mode={'disabled'}>
+          다음으로
+        </Button>
+      )}
     </StyledSignUpAgree>
   );
 };
