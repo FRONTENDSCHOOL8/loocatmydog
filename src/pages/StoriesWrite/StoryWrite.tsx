7c81eef84e000f2a3d28895589eb1c864eee31f9
jump6746 <jump6746@naver.com>
2024-03-14 01:29:29 +0900

    [BugFix] StoryWrite 이미지 데이터
    - StoryWrite 이미지 데이터 비우는 코드 작성

--- src/pages/StoriesWrite/StoryWrite.tsx
+++ src/pages/StoriesWrite/StoryWrite.tsx
@@ -252,12 +252,12 @@ export async function storyFormAction({ request }: { request: any }) {
 
   try {
     await pb.collection('boards').create(newFormData);
-    console.log(placeId);
-    console.log(userId);
     if (placeId) {
       updateReviewed(placeId, userId);
     }
 
+    // 메모리 비우기
+    imageFiles.splice(0, imageFiles.length);
     alert('스토리 작성이 완료됐습니다.');
   } catch (error) {
     console.log('Error while writing : ', error);
